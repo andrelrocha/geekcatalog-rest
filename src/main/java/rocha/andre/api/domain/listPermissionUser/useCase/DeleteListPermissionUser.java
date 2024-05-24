eca3f62cad16cb38263e4c17139c741d489cf35a
andrelrocha <andrerocha0911@gmail.com>
2024-05-24 09:23:13 -0300

    feat: delete de listpermissionuser se for permission:read, apaga todos os registros de permissão

--- src/main/java/rocha/andre/api/domain/listPermissionUser/useCase/DeleteListPermissionUser.java
+++ src/main/java/rocha/andre/api/domain/listPermissionUser/useCase/DeleteListPermissionUser.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import rocha.andre.api.domain.listPermissionUser.DTO.ListPermissionUserDTO;
 import rocha.andre.api.domain.listPermissionUser.ListPermissionUserRepository;
+import rocha.andre.api.domain.permission.PermissionEnum;
+import rocha.andre.api.domain.permission.PermissionRepository;
 import rocha.andre.api.domain.user.UserRepository;
 import rocha.andre.api.infra.exceptions.ValidationException;
 
@@ -13,6 +15,10 @@ import java.util.UUID;
 public class DeleteListPermissionUser {
     @Autowired
     private ListPermissionUserRepository repository;
+    @Autowired
+    private PermissionRepository permissionRepository;
+    @Autowired
+    private DeleteAllListPermissionUser deleteAllListPermissionUser;
     @Autowired
     private UserRepository userRepository;
 
@@ -24,6 +30,17 @@ public class DeleteListPermissionUser {
 
         var listIdUUID = UUID.fromString(data.listId());
         var permissionIdUUID = UUID.fromString(data.permissionId());
+
+        try {
+            var readEnum = PermissionEnum.READ.toString();
+            var permissionRead = permissionRepository.findByPermissionName(readEnum);
+            if (permissionRead.getId().equals(permissionIdUUID)) {
+                deleteAllListPermissionUser.deleteAllListPermission(data);
+                return;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Algo deu errado no processo de leitura de permissão no usecase de delete listPermission");
+        }
 
         var ownerIdUUID = UUID.fromString(data.ownerId());
         var owner = userRepository.findById(ownerIdUUID)
