42aa5d4ed82cda10fa7aaa33cb1005784e6fb8be
andrelrocha <andrerocha0911@gmail.com>
2024-06-04 14:53:34 -0300

    feat: rota de list permission user retorna todas as permissões quando o usuário dono da lista faz a query

--- src/main/java/rocha/andre/api/domain/listPermissionUser/DTO/ListPermissionUserReturnDTO.java
+++ src/main/java/rocha/andre/api/domain/listPermissionUser/DTO/ListPermissionUserReturnDTO.java
@@ -4,9 +4,9 @@ import rocha.andre.api.domain.listPermissionUser.ListPermissionUser;
 
 import java.util.UUID;
 
-public record ListPermissionUserReturnDTO(UUID id, UUID listId, UUID permissionId, UUID participantId, String participantName, UUID ownerId) {
+public record ListPermissionUserReturnDTO(UUID id, UUID listId, UUID permissionId, String permissionName, UUID participantId, String participantName, UUID ownerId) {
     public ListPermissionUserReturnDTO(ListPermissionUser listPermissionUser) {
-        this(listPermissionUser.getId(), listPermissionUser.getList().getId(), listPermissionUser.getPermission().getId(),
+        this(listPermissionUser.getId(), listPermissionUser.getList().getId(), listPermissionUser.getPermission().getId(), listPermissionUser.getPermission().getPermission(),
                 listPermissionUser.getParticipant().getId(), listPermissionUser.getParticipant().getName(), listPermissionUser.getOwner().getId());
     }
 }
--- src/main/java/rocha/andre/api/domain/listPermissionUser/useCase/GetListPermissionUserByListAndUser.java
+++ src/main/java/rocha/andre/api/domain/listPermissionUser/useCase/GetListPermissionUserByListAndUser.java
@@ -3,9 +3,15 @@ package rocha.andre.api.domain.listPermissionUser.useCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import rocha.andre.api.domain.listPermissionUser.DTO.ListPermissionUserReturnDTO;
+import rocha.andre.api.domain.listPermissionUser.ListPermissionUser;
 import rocha.andre.api.domain.listPermissionUser.ListPermissionUserRepository;
+import rocha.andre.api.domain.listsApp.ListAppRepository;
+import rocha.andre.api.domain.permission.PermissionRepository;
+import rocha.andre.api.domain.permission.useCase.GetAllPermissions;
 import rocha.andre.api.domain.user.UseCase.GetUserByTokenJWT;
+import rocha.andre.api.infra.exceptions.ValidationException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -15,12 +21,28 @@ public class GetListPermissionUserByListAndUser {
     @Autowired
     private ListPermissionUserRepository repository;
     @Autowired
+    private ListAppRepository listAppRepository;
+    @Autowired
     private GetUserByTokenJWT getUserByTokenJWT;
+    @Autowired
+    private PermissionRepository permissionRepository;
 
     public List<ListPermissionUserReturnDTO> getAllPermissionsByUserAndListID(String tokenJWT, String listId) {
         var participant = getUserByTokenJWT.getUserByID(tokenJWT);
         var participantIdUUID = UUID.fromString(participant.id());
         var listIdUUID = UUID.fromString(listId);
+        var list = listAppRepository.findById(listIdUUID)
+                .orElseThrow(() -> new ValidationException("Não foi encontrada lista para o id informado, na pesquisa de permissões"));
+
+        if (list.getUser().getId().equals(participantIdUUID)) {
+            var listPermission = new ArrayList<ListPermissionUserReturnDTO>();
+            var permissions = permissionRepository.findAllPermissions();
+            permissions.forEach(permission -> {
+                var dto = new ListPermissionUserReturnDTO(null, listIdUUID, permission.getId(), permission.getPermission(), participantIdUUID, participant.name(), participantIdUUID);
+                listPermission.add(dto);
+            });
+            return listPermission;
+        }
 
         var allPermissionsByListAndUserID = repository.findAllByParticipantIdAndListId(participantIdUUID, listIdUUID)
                 .stream()
